46cf6f9d9bcf87356c39097bf27b8c47d5fe5866
Ricardo Markiewicz <ricardo.markiewicz@gmail.com>
2020-06-04 01:41:40 -0300

    Move minSdkVersion to API 19 to match px-android
    Also migrate the plugin to flutter 1.12+

--- example/android/app/src/main/kotlin/ar/com/p39/mercado_pago_mobile_checkout_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/ar/com/p39/mercado_pago_mobile_checkout_example/MainActivity.kt
@@ -1,6 +1,6 @@
 package ar.com.p39.mercado_pago_mobile_checkout_example
 
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
