030ceaf7d470fa862d8ce21dd782cb5ce0ce3faf
Lautaro Paskevicius <paske@parsii.co>
2021-01-08 17:21:51 -0300

    refactor(example): update example project

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mercado_pago_mobile_checkout/mercado_pago_mobile_checkout.dart';
 
